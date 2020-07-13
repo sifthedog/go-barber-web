73858aaaf0725e2b8e45c9c7b0914befb7befe14
João <joaopfernandesc@gmail.com>
2020-07-12 23:05:55 -0300

    Routes finished

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackgroundImg from '../../assets/sign-up-background.png';
@@ -9,15 +9,36 @@ export const Container = styled.div`
   display: flex;
   align-items: stretch;
 `;
-export const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 
+export const Content = styled.div`
   place-content: center;
   width: 100%;
   max-width: 700px;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px)
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0)
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
   form {
     margin: 80px 0;
     width: 340px;
@@ -40,7 +61,7 @@ export const Content = styled.div`
     }
   }
   > a {
-    color: #f4ede8;
+    color: #ff9000;
     display: block;
     margin-top: 24px;
     transition: color 0.2s;
@@ -51,10 +72,57 @@ export const Content = styled.div`
       margin-right: 16px;
     }
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
   }
 `;
+
+// export const Content = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+
+//   place-content: center;
+//   width: 100%;
+//   max-width: 700px;
+
+//   form {
+//     margin: 80px 0;
+//     width: 340px;
+//     text-align: center;
+
+//     h1 {
+//       margin-bottom: 24px;
+//     }
+
+//     a {
+//       color: #f4ede8;
+//       display: block;
+//       margin-top: 24px;
+//       text-decoration: none;
+//       transition: color 0.2s;
+
+//       &:hover {
+//         color: ${shade(0.2, '#f4ede8')};
+//       }
+//     }
+//   }
+//   > a {
+//     color: #f4ede8;
+//     display: block;
+//     margin-top: 24px;
+//     transition: color 0.2s;
+
+//     display: flex;
+//     align-items: center;
+//     svg {
+//       margin-right: 16px;
+//     }
+//     &:hover {
+//       color: ${shade(0.2, '#f4ede8')};
+//     }
+//   }
+// `;
 export const Background = styled.div`
   flex: 1;
   background: url(${signUpBackgroundImg}) no-repeat center;
